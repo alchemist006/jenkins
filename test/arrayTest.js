ff74d6ba4295b306dbdec3e804edd0f442ee3d3d
Akalintha007 <93814531+Akalintha007@users.noreply.github.com>
2023-04-26 11:49:53 +0530

    added passing test cases

--- test/arrayTest.js
+++ test/arrayTest.js
@@ -3,9 +3,9 @@ const assert = require('assert');
 describe('Array', function(){
     describe('indexOf', function(){
         it('Should return -1 if the value is not in the array or a positive number (index) if it does exist', function(){
-            assert.equal([1,2,3].indexOf(4), -1);
+            // assert.equal([1,2,3].indexOf(4), -1);
             assert.equal([1,2,3].indexOf(1), 0);
-            assert.equal([1,2,3].indexOf(0), 1);
+            assert.equal([1,2,3].indexOf(2), 1);
         });
     });
 });
