32bcf310f589ac03a2a4e7346944699ada576af3
Akalintha007 <93814531+Akalintha007@users.noreply.github.com>
2023-04-26 11:58:47 +0530

    added anothe rassertion

--- test/arrayTest.js
+++ test/arrayTest.js
@@ -1,12 +1,13 @@
-const assert = require('assert');
+const assert = require("assert");
 
-describe('Array', function(){
-    describe('indexOf', function(){
-        it('Should return -1 if the value is not in the array or a positive number (index) if it does exist', function(){
-            // assert.equal([1,2,3].indexOf(4), -1);
-            assert.equal([1,2,3].indexOf(1), 0);
-            assert.equal([1,2,3].indexOf(2), 1);
-            assert.equal([1,2,3].indexOf(3), 2);
-        });
+describe("Array", function () {
+  describe("indexOf", function () {
+    it("Should return -1 if the value is not in the array or a positive number (index) if it does exist", function () {
+      // assert.equal([1,2,3].indexOf(4), -1);
+      assert.equal([1, 2, 3].indexOf(1), 0);
+      assert.equal([1, 2, 3].indexOf(2), 1);
+      assert.equal([1, 2, 3].indexOf(3), 2);
+      assert.equal([1, 2, 4].indexOf(4), 2);
     });
+  });
 });
